dd3daab824a25f5271f9be22f7ffa9a949259325
Arne Naegel <naegel@gcsc.uni-frankfurt.de>
2022-12-12 22:11:22 +0100

    Use ug4pybind_add_module

--- limex_plugin.h
+++ limex_plugin.h
@@ -5,6 +5,9 @@
 extern "C" void InitUGPlugin_Limex(ug::bridge::Registry* reg, std::string grp);
 
 #ifdef UG_USE_PYBIND11
+
+#include "bindings/pybind/ug_pybind.h"
+
 namespace ug {
 namespace Limex{
 	void InitUGPlugin(ug::pybind::Registry* reg, std::string grp);
